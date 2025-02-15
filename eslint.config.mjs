b7fea773a0dd0d1451db8ea1bac62e633eec8623
aahmedrradwan00 <ahmed.ibrahem1101@gmail.com>
2025-02-15 21:38:53 +0200

    try swagger

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,35 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-export default tseslint.config(
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import globals from 'globals';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
   {
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: eslintPluginPrettier,
+    },
     languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: process.cwd(), // Use process.cwd() instead of import.meta.dirname
+      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
-  },
-  {
     rules: {
+      'prettier/prettier': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
@@ -35,4 +43,43 @@ export default tseslint.config(
       ],
     },
   },
-);
+];
+
+// import swaggerJsdoc from 'swagger-jsdoc';
+// export default tseslint.config(
+//   {
+//     ignores: ['eslint.config.mjs'],
+//   },
+//   eslint.configs.recommended,
+//   ...tseslint.configs.recommendedTypeChecked,
+//   eslintPluginPrettierRecommended,
+//   {
+//     languageOptions: {
+//       globals: {
+//         ...globals.node,
+//         ...globals.jest,
+//       },
+//       ecmaVersion: 5,
+//       sourceType: 'module',
+//       parserOptions: {
+//         projectService: true,
+//         tsconfigRootDir: import.meta.dirname,
+//       },
+//     },
+//   },
+//   {
+//     rules: {
+//       '@typescript-eslint/no-explicit-any': 'off',
+//       '@typescript-eslint/no-floating-promises': 'warn',
+//       '@typescript-eslint/no-unsafe-argument': 'warn',
+//
+//       // Allow unsafe assignment in swaggerConfig.ts
+//       '@typescript-eslint/no-unsafe-assignment': [
+//         'warn',
+//         {
+//           ignoreProperties: true,
+//         },
+//       ],
+//     },
+//   },
+// );
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('hi')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -10,6 +19,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @ApiResponse({ status: 201, description: 'Hello World!' })
   @Get('hi')
   firstHiInProject(): string {
     return this.appService.firstHiInProject();
