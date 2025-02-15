5432b7eb7e0f35242783ac93f21a89a120e5d51d
Abdelrahman Adel <95689929+Abdelrahman1625@users.noreply.github.com>
2025-02-15 20:41:01 +0200

    Add Swagger and edit main.ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,4 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-
+import swaggerJsdoc from 'swagger-jsdoc';
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
@@ -29,7 +24,15 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+
+      // Allow unsafe assignment in swaggerConfig.ts
+      '@typescript-eslint/no-unsafe-assignment': [
+        'warn',
+        {
+          ignoreProperties: true,
+        },
+      ],
     },
   },
 );
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Buy From Egypt API')
+    .setDescription('API Documentation for Buy From Egypt')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log('📄 Swagger Docs available at: http://localhost:3000/api-docs');
 }
 
 bootstrap();
