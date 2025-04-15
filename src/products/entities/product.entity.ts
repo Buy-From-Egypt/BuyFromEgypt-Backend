f35ae8c9e9cf1eb02de920bc531ebf0b51baaf90
aahmedrradwan00 <ahmed.ibrahem1101@gmail.com>
2025-04-15 14:57:48 +0200

    init category resource

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,10 +1,11 @@
 import { ProductImage } from './productImage.entity';
 import { User } from '../../users/entities/user.entity';
+import { Category } from '../../categories/entities/category.entity';
 
 export class Product {
   productId: string;
   name: string;
-  slug?: string;
+  slug: string;
   description?: string | null;
   price: number;
   currencyCode: string;
@@ -12,13 +13,14 @@ export class Product {
   rating?: number | null;
   reviewCount: number;
   owner?: User;
-  // category?: Category;
-  // approvedBy?: User;
-  // approvedAt?: Date;
+  category?: Category;
+  approvedBy?: User;
+  approvedAt?: Date | null;
   // certifications: Certification[];
   // orders: Order[];
   // posts: Post[];
   images?: ProductImage[];
+  cloudFolder?: string | null
   createdAt: Date;
   updatedAt: Date;
 }
