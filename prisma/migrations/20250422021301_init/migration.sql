17b404068429fc0213b0ffc7f5bfb45bb46ab710
Abdelrahman1625 <abdelrahman.adel063@gmail.com>
2025-04-22 04:15:59 +0200

    refactor: remove obsolete migration files for password reset and comment table changes

--- prisma/migrations/20250422021301_init/migration.sql
+++ prisma/migrations/20250422021301_init/migration.sql
@@ -82,13 +82,13 @@ CREATE TABLE "PostLike" (
 
 -- CreateTable
 CREATE TABLE "Comment" (
-    "id" TEXT NOT NULL,
+    "commentId" TEXT NOT NULL,
     "postId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("commentId")
 );
 
 -- CreateTable
